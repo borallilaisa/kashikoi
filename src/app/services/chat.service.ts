80a7e0e1685382f47316b9456ae4e90ed4589cc2
Rodrigo Lima <rodrigoscarpimlima@hotmail.com>
2021-01-09 16:24:44 -0300

    Chat

---
+++ src/app/services/chat.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../environments/environment';
+import { UserServiceService } from './user-service.service';
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  constructor(public http: HttpClient,
+              public userService: UserServiceService) {}
+
+  openChat(id_professor, id_aluno) {
+    let user:any = this.userService.getAuthUser();
+
+    return new Promise((resolve, reject) => {
+
+      this.http.get( `${environment.appUrl}/chat/abrir/${id_professor}/${id_aluno}?token=${user.token}`)
+        .subscribe((data:any) => resolve(data), (err:any) => reject(err));
+
+    });
+  }
+
+  getMessages(chat_id, page = 1) {
+
+    let user:any = this.userService.getAuthUser();
+
+    return new Promise((resolve, reject) => {
+
+      this.http.get( `${environment.appUrl}/chat/${chat_id}/mensagens?page=${page}&token=${user.token}`)
+        .subscribe((data:any) => resolve(data), (err:any) => reject(err));
+
+    });
+
+  }
+
+  sendMessage(chat_id, data) {
+    let user:any = this.userService.getAuthUser();
+
+    return new Promise((resolve, reject) => {
+
+      this.http.post( `${environment.appUrl}/chat/${chat_id}/mensagens?token=${user.token}`, data)
+        .subscribe((data:any) => resolve(data), (err:any) => reject(err));
+
+    });
+  }
+}
