465962db77fa79a571692aafb3727316db9b9422
laisa boralli <borallilaisa@gmail.com>
2021-03-29 20:01:30 -0300

    pusher

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   appUrl: 'https://kashikoi-api.herokuapp.com/api',
   authUrl: 'https://kashikoi-api.herokuapp.com',
-  pusher_app_id: '1135448',
-  pusher_app_key: '9424a784cdc7d4687c0e',
-  pusher_app_secret: '190410ae1ac2260d4d78',
+  pusher_app_id: '1135450',
+  pusher_app_key: '54828454c77111a4e20f',
+  pusher_app_secret: 'a976c0475a82eb1b918d',
   pusher_app_cluster: 'us2',
   facebook_client_id: '810127743051037',
   google_client_id: '125674583143-qlqg103pfc5uta2kpsf4782ps1fdhale.apps.googleusercontent.com'
