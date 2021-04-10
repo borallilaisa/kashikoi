4cedcc5caba162da0b70e91d5603016d4aaeba72
laisa boralli <borallilaisa@gmail.com>
2021-04-10 18:28:00 -0300

    nova chave facebook

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -6,6 +6,6 @@ export const environment = {
   pusher_app_key: '54828454c77111a4e20f',
   pusher_app_secret: 'a976c0475a82eb1b918d',
   pusher_app_cluster: 'us2',
-  facebook_client_id: '810127743051037',
+  facebook_client_id: '296715471826078',
   google_client_id: '125674583143-qlqg103pfc5uta2kpsf4782ps1fdhale.apps.googleusercontent.com'
 };
